31a1cedfedddf034138b504cd6820f4aaab48694
Nicolas-SFranca <nicolasdifranc@gmail.com>
2024-08-19 23:47:05 -0300

    Create exercicio08.py

---
+++ Algoritimos_interativos/exercicio08.py
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SomaNumeros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int N = scanner.nextInt();
+        int soma = 0;
+        for (int i = 1; i <= N; i++) {
+            soma += i;
+        }
+        System.out.println("A soma dos números anteriores a " + N + " é: " + soma);
+        scanner.close();
+    }
+}
