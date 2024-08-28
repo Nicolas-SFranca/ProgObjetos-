68af72b361ef760991fbe115ef4a5787dd4259b1
Nicolas-SFranca <nicolasdifranc@gmail.com>
2024-08-28 11:32:52 -0300

    Create Cliente.java

---
+++ lista02/Exercio01/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    String nome;
+    String cpf;
+    String endereco;
+
+    public Cliente(String nome, String cpf, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+}
