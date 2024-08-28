69fa091a611d3df8f189168d02f4111ec30d1c68
Nicolas-SFranca <nicolasdifranc@gmail.com>
2024-08-28 15:33:35 -0300

    Create ItemPedido.java

---
+++ lista02/exercicio08/ItemPedido.java
@@ -0,0 +1,11 @@
+public class ItemPedido {
+  String produto;
+  int quantidade;
+  double preco;
+  
+  public ItemPedido(String produto, int quantidade, double preco){
+    this.produto = produto;
+    this.quantidade = quantidade;
+    this.preco = preco;
+  }
+}
