58a4b4bede37935a51e5c45ecf14c88bd971b767
Nicolas-SFranca <nicolasdifranc@gmail.com>
2024-09-04 21:48:09 -0300

    Create Principal.java

---
+++ lista4/exercicio01/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    // Método principal (main)
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        c1.setMarca("LOGAN");
+        c1.setModelo("novo");
+        c1.setAno(2020);
+        c1.setPreco(18000);
+        
+        c1.exibir();
+        
+    }    
+}
